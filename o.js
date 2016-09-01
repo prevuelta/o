debd37ea9c74fd71ae4c317f58bf8682991eb9ec
Pablo <Pablo@Deans-iMac.local>
2016-09-01 15:24:54 +1200

    rejig

--- o.js
+++ o.js
@@ -1,10 +1,39 @@
 'use strict';
 
 function o (selector) {
-    if (!selector) {
+
+    if (!selector || typeof selector !== "string") {
         return;
     }
-    if (!!~selector.indexOf('#')) {
-        return document.getElementById(selector);
+
+    let matches = new RegExp(/#|\.|\[|[a-z]/).exec(selector);
+
+    if (matches) {
+
+        if (matches[0] === '#') {
+            return document.getElementById(selector);
+        }
+
+        if (matches[0] === '.' || matches[0] === '[') {
+            return document.querySelectorAll(selector);
+
+        }
+
+        // Match tag
+        if (matches[0]) {
+            return document.getElementByTagName(matches[0]);
+        }
+
+    } else {
+        return;
     }
 }
+
+o.addListenersToNodeList = function (nodeArray, callback, event) {
+    [].slice.call(nodeArray).forEach((el) => {
+        el.addEventListener(event ? event : 'click', callback);
+    });
+};
+
+
+module.exports = o;
